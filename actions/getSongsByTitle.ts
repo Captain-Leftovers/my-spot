5ca8ce4db58b3b13ca8a6f977c9fff819a933ce4
Dob <lalilulelo.mg@gmail.com>
2023-06-30 15:48:50 +1000

    added mediaItem component

---
+++ actions/getSongsByTitle.ts
@@ -0,0 +1,30 @@
+import type { Song } from '@/types'
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+import { toast } from 'react-hot-toast'
+import getSongs from './getSongs'
+
+const getSongsByTitle = async (title: string): Promise<Song[]> => {
+	const supabase = createServerComponentClient({
+		cookies: cookies,
+	})
+
+	if (!title) {
+		const allSongs = await getSongs()
+		return allSongs
+	}
+
+	const { data, error } = await supabase
+		.from('songs')
+		.select('*')
+		.ilike('title', `%${title}%`)
+		.order('created_at', { ascending: false })
+
+	if (error) {
+		toast.error(error.message)
+	}
+
+	return (data as Song[]) || []
+}
+
+export default getSongsByTitle
