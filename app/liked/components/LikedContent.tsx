e3714eaa18378cf0b2eb81a9ceeb6f61fae0278e
Dob <lalilulelo.mg@gmail.com>
2023-07-03 09:16:56 +1000

    finished lkiked content part added madia item

--- app/liked/components/LikedContent.tsx
+++ app/liked/components/LikedContent.tsx
@@ -1,38 +1,42 @@
 'use client'
 
-import { useUser } from "@/hooks/useUser"
-import { Song } from "@/types"
-import { useRouter } from "next/navigation"
-import { useEffect } from "react"
-
-
-
+import LikeButton from '@/components/LikeButton'
+import MediaItem from '@/components/MediaItem'
+import { useUser } from '@/hooks/useUser'
+import { Song } from '@/types'
+import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
 
 type LikedContentProps = {
-    songs:Song[]
-  }
+	songs: Song[]
+}
 export default function LikedContent({ songs }: LikedContentProps) {
+	const router = useRouter()
+	const { isLoading, user } = useUser()
 
+	useEffect(() => {
+		if (!isLoading && !user) {
+			router.replace('/')
+		}
+	}, [isLoading, user, router])
 
-    const router = useRouter()
-    const { isLoading, user, } = useUser()
+	if (songs.length === 0)
+		return (
+			<div className="flex flex-col gap-y-2 w-full px-6 text-neutral-400">
+				No Liked Songs
+			</div>
+		)
 
-    useEffect(() => {
-        if (!isLoading && !user) {
-            router.replace('/')
-        } 
-    }, [isLoading, user, router])
-
-
-    if(songs.length === 0) return (
-     <div className="flex flex-col gap-y-2 w-full px-6 text-neutral-400">
-        No Liked Songs
-     </div>
-    )
-
-return (
-  <div>
-     LikedContent
-  </div>
-  )
+	return (
+		<div className="flex flex-col gap-y-2 w-full p-6">
+			{songs.map((song) => (
+				<div key={song.id} className="flex items-center gap-x-4 w-full">
+					<div className="flex-1">
+						<MediaItem data={song} onClick={() => {}} />
+					</div>
+					<LikeButton songId={song.id} />
+				</div>
+			))}
+		</div>
+	)
 }
