938ba64f4f83658d7591db02e9ee1e6cd2f1da5f
Dob <lalilulelo.mg@gmail.com>
2023-07-06 12:15:40 +1000

    added fix on logout to close player

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,12 +11,14 @@ import { useSupabaseClient } from '@supabase/auth-helpers-react'
 import { useUser } from '@/hooks/useUser'
 import { FaUserAlt } from 'react-icons/fa'
 import { toast } from 'react-hot-toast'
+import usePlayer from '@/hooks/usePlayer'
 
 type Props = {
 	children: React.ReactNode
 	className?: string
 }
 export default function Header({ children, className }: Props) {
+	const player = usePlayer()
 	const router = useRouter()
 	const authModal = useAuthModal()
 	const supabaseClient = useSupabaseClient()
@@ -25,7 +27,7 @@ export default function Header({ children, className }: Props) {
 	const handleLogout = async () => {
 		const { error } = await supabaseClient.auth.signOut()
 
-		//TODO : reset playing songs when implemented
+		player.reset()
 		router.refresh()
 
 		if (error) {
