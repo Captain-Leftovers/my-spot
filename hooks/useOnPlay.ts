24022a7360e6a514df9d676cbd5a080d3b043d0d
Dob <lalilulelo.mg@gmail.com>
2023-07-07 10:02:26 +1000

    removed subscription needed to listen to music

--- hooks/useOnPlay.ts
+++ hooks/useOnPlay.ts
@@ -1,35 +1,23 @@
-import { Song } from "@/types";
-import usePlayer from "./usePlayer";
-import useAuthModal from "./useAuthModal";
-import { useUser } from "./useUser";
-import useSubscribeModal from "./useSubscribeModal";
+import { Song } from '@/types'
+import usePlayer from './usePlayer'
+import useAuthModal from './useAuthModal'
+import { useUser } from './useUser'
 
+const useOnPlay = (songs: Song[]) => {
+	const player = usePlayer()
+	const authModal = useAuthModal()
+	const { user } = useUser()
 
+	const onPlay = (id: string) => {
+		if (!user) {
+			return authModal.onOpen()
+		}
 
+		player.setId(id)
+		player.setIds(songs.map((song) => song.id))
+	}
 
-const useOnPlay = (songs:Song[]) =>  {
-
-    const subscribeModal = useSubscribeModal()
-    const player = usePlayer()
-    const authModal = useAuthModal()
-    const { user, subscription } = useUser()
-
-    const onPlay = (id:string) => {
-        if (!user) {
-            return authModal.onOpen()
-        }
-
-        if (!subscription) {
-            return  subscribeModal.onOpen()
-        }
-
-        player.setId(id)
-        player.setIds(songs.map(song => song.id))
-    }
-
-
-    return onPlay
-} 
-
+	return onPlay
+}
 
 export default useOnPlay
